d9d05453ec5635d333a43efc33a7d27b2944508a
zhanbolat <zhanbolat.mukan2004@gmail.com>
2022-06-15 14:22:16 +0600

    asdrw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import icon from "./img/icon.png";
+import { format, compareAsc } from 'date-fns'
 
+
+let time = format(new Date(), 'dd/MMM/yyyy HH:mm:ss')
+// let hours = format(new Date())
+// let wholetime = time + " " + hours;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className = "headDiv">
+              <img src= {icon} alt = "icon" className='icon'/>
+              <input type = "text" placeholder='search'/>
+          </div>
+          <div className='time'>
+              {time}
+          </div>
       </header>
+      <div className='mainDiv'>
+        <div className = 'firstDiv'>
+           <h2>Hey</h2>  
+        </div>
+        <div className = 'secondDiv'>
+           <h2>Let's</h2>
+        </div>
+        <div className = 'thirdDiv'>
+           <h2>Give</h2>
+        </div>      
+        <div className = 'fourthDiv'>
+           <h2>All</h2>
+        </div>
+        <div className = 'fifthDiv'>
+           <h2>You Can</h2>
+        </div>
+        
+         
+      </div>
+     
+         
+    
     </div>
   );
 }
